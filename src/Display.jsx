d07214cbcd6f159680a41a520b3b8d60ba5cd27c
mylastresort <stamim@student.1337.ma>
2023-09-25 18:08:46 +0100

    adding more styles

--- src/Display.jsx
+++ src/Display.jsx
@@ -16,28 +16,27 @@ export default function Display() {
     <>
       <div>{current.length} results</div>
       <div className="display">
-        {current.map(({ images, id }) => (
-          <picture
-            key={id}
-            style={{
-              aspectRatio:
-                images.fixed_width_downsampled.width /
-                images.fixed_width_downsampled.height,
-            }}
-            className="gif"
-          >
-            {images.fixed_width_downsampled.webp && (
-              <source
-                srcSet={images.fixed_width_downsampled.webp}
-                type="image/webp"
+        {current.map(({ images, id, alt_text }) => (
+          <a key={id} href={images.fixed_width.url} target="_blank">
+            <picture
+              className="gif"
+              style={{
+                aspectRatio:
+                  images.fixed_width.width / images.fixed_width.height,
+              }}
+            >
+              {images.fixed_width.webp && (
+                <source srcSet={images.fixed_width.webp} type="image/webp" />
+              )}
+              <img
+                alt={alt_text}
+                loading="lazy"
+                onLoad={(e) => (e.target.style.opacity = 1)}
+                src={images.fixed_width.url}
               />
-            )}
-            <img
-              loading="lazy"
-              onLoad={(e) => (e.target.style.opacity = 1)}
-              src={images.fixed_width_downsampled.url}
-            />
-          </picture>
+              <div className="overlay" />
+            </picture>
+          </a>
         ))}
       </div>
     </>
