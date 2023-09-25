dff59fe1a08c0aa7856d29df63a3d9ac25f21448
mylastresort <stamim@student.1337.ma>
2023-09-25 10:55:26 +0100

    Picture onload event removed and component is inlined

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,6 +1,5 @@
 import "./Display.css";
 import { useSelector } from "react-redux";
-import Picture from "./Picture";
 
 export default function Display() {
   const current = useSelector((state) => state.giphy.current);
@@ -17,8 +16,18 @@ export default function Display() {
     <>
       <div>{current.length} results</div>
       <div className="display">
-        {current.map((item) => (
-          <Picture images={item.images} id={item.id} key={item.id} />
+        {current.map(({ images, id }) => (
+          <picture
+            key={id}
+            style={{
+              aspectRatio:
+                images.fixed_width_downsampled.width /
+                images.fixed_width_downsampled.height,
+            }}
+            className="gif"
+          >
+            <img loading="lazy" src={images.fixed_width_downsampled.url} />
+          </picture>
         ))}
       </div>
     </>
